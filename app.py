db7e02053a52837119de707f7b1004c65bc38bdf
NikitaBarsukov <nik0xff@gmail.com>
2017-06-20 18:15:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__, static_url_path='/static')
 @app.route("/")
 def index():
     return app.send_static_file('index.html')
+
+@app.route("/")
+def index():
+    return redirect("https://drive.google.com/file/d/0B1ZVeCv9HaaJZ002WDNFM0Znd2s/view", code=302)
